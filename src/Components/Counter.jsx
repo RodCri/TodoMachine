039f7a9f352ca61aea4b3500034bd65bf706bca5
RodCri <cristian.rodriguez1317@gmail.com>
2024-01-22 10:28:21 -0500

    Change menssage completed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 import './Counter.css';
 
 export const Counter = ({completed = 0, total = 0}) => {
+  const mensaje = completed === total ? "¡Completaste todos tus TODOs!" : "";
   return (
     <p className="counter">
       Has completado <span className='counter__number'>{completed}</span> de <span className='counter__number'> {total} </span> TODOs
