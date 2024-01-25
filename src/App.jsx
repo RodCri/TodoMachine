465339670bf086e35f346ff5b78dfdee8893fdf2
RodCri <cristian.rodriguez1317@gmail.com>
2024-01-25 09:42:47 -0500

    create custom hook for localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { Counter } from './Components/Counter'
 import { Filter } from './Components/Filter'
 import { Task } from './Components/Task'
 import { ListTasks } from './Components/ListTasks'
 import { NewItem } from './Components/NewItem'
+import { useLocalStorage } from './Components/useLocalStorage'
 
 function App() {
-
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-  
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
   // eslint-disable-next-line no-unused-vars
-  const [listTasks, setListTasks] = useState(parsedTodos);
+  const [listTasks, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = useState("");
 
   const completedTodos = listTasks.filter(task => task.complete).length;
   const totalTodos = listTasks.length;
-
-  useEffect(() => {
-    if (localStorage.getItem("TODOS_V1")) {
-      if (JSON.parse(localStorage.getItem("TODOS_V1")).length > 0) {
-        setListTasks(JSON.parse(localStorage.getItem("TODOS_V1")));
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    const savedData = [...listTasks];
-    localStorage.setItem("TODOS_V1", JSON.stringify(savedData));
-  }, [listTasks]);
-
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-    setListTasks(newTodos);
-  };
 
   const searchedTodos = listTasks.filter((task)=>{
     // función texto sin tildes
     const noTildes = (text) => {
       return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     };
-
     // Normalizando texto sin tildes y a Lower Case
     const TodoTextLC = noTildes(task.taskName.toLowerCase());
     const searchTextLC = noTildes(searchValue.toLowerCase());
-
     //renderizar con filtro
     return TodoTextLC.includes(searchTextLC);
   })
@@ -88,5 +58,4 @@ function App() {
     </div>
   )
 }
-
 export default App
